6b4190da0f7da2f431580c4bd846d46cdc686b70
Lucas Stasiak <108033145+StasiakLucas@users.noreply.github.com>
2024-05-27 12:26:38 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Programa feito para calcular a velocidade média em km/h");
+    System.out.println();
+    System.out.println("A distância considerada é:");
+    double distancia = 400;
+    System.out.println(distancia);
+    System.out.println();
+
+    System.out.println("O tempo considerado é:");
+    double tempo = 3;
+    System.out.println(tempo);
+    System.out.println();
+    double velocidadeMedia = distancia * tempo;
+
+    System.out.println("A velocidade média é:");
+    System.out.println(velocidadeMedia);
+  }
+}
